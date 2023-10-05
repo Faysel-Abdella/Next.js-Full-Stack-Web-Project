37896b49d806fa6b112161be9a54a809351b1b9c
faysel abdella <fayselcode@gmail.com>
2023-10-05 15:07:20 -0500

    A product api is added

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import schema from "./schema";
+
 export function GET(request: NextRequest) {
     return NextResponse.json([
         {
@@ -13,4 +15,19 @@ export function GET(request: NextRequest) {
             price: 3.5
         }
     ])
+}
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+
+    const validationResult = schema.safeParse(body)
+
+    if(!validationResult.success)
+        return NextResponse.json(validationResult.error.errors, {status: 400})
+
+    return NextResponse.json({
+        id: 10,
+        name: body.name,
+        price: body.price
+    })
 }
