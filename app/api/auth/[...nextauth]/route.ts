3e6c4d5d0fe6eb49d6b6529213c5cba525d9d459
faysel abdella <fayselcode@gmail.com>
2023-10-06 14:47:45 +0300

    the strategy of the session method is spesified to be jwt

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,18 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
 
         })
-    ]
+    ],
+    session: {
+        strategy: 'jwt'
+    }
+
 }
 
 
